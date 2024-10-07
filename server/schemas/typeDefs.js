d120cef824bff480bb23e0cbff42ccacd67e6612
ndoppler <112019993+ndoppler@users.noreply.github.com>
2024-10-06 19:35:15 -0500

    changes in the backend (typedefs and resolvers)

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const typeDefs = `
+type Auth {
+    token: ID!
+    me: User
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]    
+}
+
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+}
+
+type Query {
+    user: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+
+    addUser(username: String!, email: String!, password: String!): Auth
+
+    saveBook(authors: [String], description: String!, title: String!, bookId: String!, image: String, link: String): User
+
+    removeBook(bookId: ID!): User
+}
+`;
+
+module.exports = typeDefs;
