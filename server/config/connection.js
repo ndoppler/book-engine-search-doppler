d23a5b0d8c37322ba21e4078742931ff26dccf52
ndoppler <112019993+ndoppler@users.noreply.github.com>
2024-10-09 20:14:48 -0500

    revert connection.js file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookSearchEngine', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Successfully connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
