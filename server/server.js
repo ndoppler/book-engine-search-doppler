7e81f0ed669ae3e8735706e58ba80237090213b9
ndoppler <112019993+ndoppler@users.noreply.github.com>
2024-10-09 19:40:31 -0500

    changes to server.js file

--- server/server.js
+++ server/server.js
@@ -1,38 +1,38 @@
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+const dns = require('dns');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Apollo Server setup
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
+  cache: "bounded", 
+  persistedQueries: false,
 });
 
-const startApolloServer = async () => {
+async function startApolloServer() {
   await server.start();
-  
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-  
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+  server.applyMiddleware({ app });
 
-  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
+  }
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  } 
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
