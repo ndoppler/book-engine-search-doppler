4a91db3724ef186c8cf7af14cb04e0c2cbac1e7c
ndoppler <112019993+ndoppler@users.noreply.github.com>
2024-10-09 19:46:54 -0500

    changes to server file because render fail

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bookSearchEngine', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
